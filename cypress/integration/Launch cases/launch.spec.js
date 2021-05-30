a2eb01ed660a38e0b7910b3a3f989c6ccbee51ff
Nikhil Varma <nikhilvarma892@gmail.com>
2021-05-30 22:23:36 +0530

    chore: Integrate Cypress

---
+++ cypress/integration/Launch cases/launch.spec.js
@@ -0,0 +1,55 @@
+/// <reference types="cypress" />
+
+describe("Launch cards functionality", () => {
+  const baseUrl = "http://localhost:3000";
+  const pageUrl = `${baseUrl}/launches/108`;
+
+  before(() => {
+    cy.visit(pageUrl);
+  });
+
+  it("should load the banner image and header detail", () => {
+    cy.get("[data-testid=launchImageBanner]").should("exist");
+    cy.get("[data-testid=launchHeader]").contains(
+      "Sentinel-6 Michael Freilich"
+    );
+  });
+
+  it("should load the flight number and launch status", () => {
+    cy.get("[data-testid=launchFlightNumber]").contains("#108");
+    cy.get("[data-testid=launchStatus]").contains("Successful");
+  });
+
+  it("should load the launch details", () => {
+    cy.get("[data-testid=launchStatus]").contains("Successful");
+  });
+
+  it("should load the breadcrumbs for the page", () => {
+    cy.get("[data-testid=homeLink]").should("exist");
+    cy.get("[data-testid=launchesLink]").should("exist");
+    cy.get("[data-testid=launchItemLink]").should("exist");
+  });
+
+  it("should have the site name navigation link", () => {
+    cy.get("[data-testid=launchSiteName]").contains("VAFB SLC 4E");
+    cy.get("[data-testid=launchSite]").click();
+    cy.url().should("include", "launch-pads/vafb_slc_4e");
+    cy.go("back");
+  });
+
+  it("should load rocket info", () => {
+    cy.get("[data-testid=launchRocketName]").contains("Falcon 9");
+    cy.get("[data-testid=launchFirstStage]").contains("B1063");
+    cy.get("[data-testid=launchFirstStageStatus]").contains("Recovered");
+    cy.get("[data-testid=launchSecondStage]").contains("Block 5");
+    cy.get("[data-testid=launchPayload]").contains("Payload: Satellite");
+  });
+
+  it("should load the launch video", () => {
+    cy.get("[data-testid=launchVideo").should("be.visible");
+  });
+
+  it("should load the launch gallery", () => {
+    cy.get("[data-testid=launchImageGallery").should("be.visible");
+  });
+});
---
+++ cypress/integration/Launch cases/launches.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe("Launches Page functionality", () => {
+  const baseUrl = "http://localhost:3000";
+  const pageUrl = `${baseUrl}/launches`;
+  before(() => {
+    cy.visit(pageUrl);
+  });
+
+  it("should load the launches page properly assuming the API works", () => {
+    cy.get("[data-testid=errorMessage]").should("not.exist");
+  });
+
+  it("should show more button at the end of the launches list", () => {
+    cy.get("[data-testid=loadMoreButton]").should("exist");
+  });
+
+  it("should display breadcrumbs in the right order", () => {
+    cy.get("[data-testid=homeLink]").should("exist");
+    cy.get("[data-testid=launchesLink]").should("exist");
+  });
+
+  it("should load the next page for the launch items", () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000);
+    cy.get("[data-testid=loadMoreButton]").click();
+    cy.get("[data-testid=launchItems]").find("a").should("have.length", 24);
+  });
+
+  it("should nagivate to launch item page", () => {
+    cy.get("[data-testid=launchItems]").find("a:first").click();
+    cy.url().should("include", "/launches");
+    cy.go("back");
+  });
+});
